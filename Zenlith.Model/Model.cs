822c0e9a67e5ca5b26cf812bfdec2de283139251
Gokcer Gokdal <gokcer@armikom.com>
2023-12-21 10:11:58 +0300

    Added model project

---
+++ Zenlith.Model/Model.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Zenlith.Model;
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public byte[]  Photo { get; set; }
+    public decimal Height { get; set; }
+    public float Weight { get; set; }
+        
+    public Grade Grade { get; set; }
+}
+
+public class Grade
+{
+    public int GradeId { get; set; }
+    public string GradeName { get; set; }
+    public string Section { get; set; }
+    
+    public ICollection<Student> Students { get; set; }
+}
+
+public class SchoolContext: DbContext 
+{
+    public SchoolContext(): base()
+    {
+            
+    }
+            
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+}
